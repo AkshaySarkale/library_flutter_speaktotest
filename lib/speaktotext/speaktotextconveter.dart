f0084e96bb1a5e5cc5b89860a09410f5b54eb83d
AkshaySarkale <akshaysarkale999@gmail.com>
2026-06-02 13:21:07 +0530

    added demo

---
+++ lib/speaktotext/speaktotextconveter.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class Speaktotextconveter extends StatefulWidget {
+  const Speaktotextconveter({super.key});
+
+  @override
+  State<Speaktotextconveter> createState() => _SpeaktotextconveterState();
+}
+
+class _SpeaktotextconveterState extends State<Speaktotextconveter> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Column(
+      children: [
+        Text("Demo"),
+      ],
+    ));
+  }
+}
